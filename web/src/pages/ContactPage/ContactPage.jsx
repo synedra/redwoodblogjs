991cf33b20207cdd1f2654c91c363f1caca41e7b
synedra-datastax <synedra@gmail.com>
2023-08-15 15:30:20 -0700

    First commit

---
+++ web/src/pages/ContactPage/ContactPage.jsx
@@ -0,0 +1,73 @@
+import { MetaTags, useMutation } from '@redwoodjs/web'
+import {
+  FieldError,
+  Form,
+  Label,
+  TextField,
+  TextAreaField,
+  Submit,
+} from '@redwoodjs/forms'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
+const ContactPage = () => {
+  const [create] = useMutation(CREATE_CONTACT)
+
+  const onSubmit = (data) => {
+    create({ variables: { input: data } })
+  }
+
+  return (
+    <>
+      <MetaTags title="Contact" description="Contact page" />
+
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+  name="email"
+  validation={{
+    required: true,
+    pattern: {
+      value: /^[^@]+@[^.]+\..+$/,
+      message: 'Please enter a valid email address',
+    },
+  }}
+  errorClassName="error"
+/>
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
+        <Submit>Save</Submit>
+      </Form>
+    </>
+  )
+}
+
+export default ContactPage
